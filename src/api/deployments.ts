92cd6b01864690ddbc0a971f619d4f51d25f67db
aarlaud <antoine@snyk.io>
2024-08-09 11:06:56 -0700

    fix: wrap up deployments command (#3)

--- src/api/deployments.ts
+++ src/api/deployments.ts
@@ -6,6 +6,13 @@ import {createLogger} from '../utils/logger.js'
 
 const logger = createLogger('snyk-broker-config')
 
+export interface DeploymentAttributesMetadata {
+  [key: string]: any
+}
+export interface DeploymentAttributes {
+  metadata: DeploymentAttributesMetadata
+}
+
 export const getDeployments = async (tenantId: string, installId: string) => {
   const headers = {...commonHeaders, ...getAuthHeader()}
   const apiPath = `rest/tenants/${tenantId}/brokers/installs/${installId}/deployments`
@@ -15,6 +22,80 @@ export const getDeployments = async (tenantId: string, installId: string) => {
     url: `${config.API_HOSTNAME}/${apiPath}?version=${config.API_VERSION}`,
     headers: headers,
     method: 'GET',
+  }
+  const response = await makeRequest(req)
+  logger.debug({statusCode: response.statusCode, response: response.body}, 'Response')
+  return response.body
+}
+
+export const createDeployment = async (
+  tenantId: string,
+  installId: string,
+  deploymentAttributes: DeploymentAttributesMetadata,
+) => {
+  const headers = {...commonHeaders, ...getAuthHeader()}
+  const apiPath = `rest/tenants/${tenantId}/brokers/installs/${installId}/deployments`
+  const config = getConfig()
+  const body = {
+    data: {
+      type: 'broker_deployment',
+      attributes: {
+        metadata: {
+          ...deploymentAttributes,
+        },
+      },
+    },
+  }
+  const req: HttpRequest = {
+    url: `${config.API_HOSTNAME}/${apiPath}?version=${config.API_VERSION}`,
+    headers: headers,
+    method: 'POST',
+    body: JSON.stringify(body),
+  }
+  const response = await makeRequest(req)
+  logger.debug({statusCode: response.statusCode, response: response.body}, 'Response')
+  return response.body
+}
+
+export const deleteDeployment = async (tenantId: string, installId: string, deploymentId: string) => {
+  const headers = {...commonHeaders, ...getAuthHeader()}
+  const apiPath = `rest/tenants/${tenantId}/brokers/installs/${installId}/deployments/${deploymentId}`
+  const config = getConfig()
+
+  const req: HttpRequest = {
+    url: `${config.API_HOSTNAME}/${apiPath}?version=${config.API_VERSION}`,
+    headers: headers,
+    method: 'DELETE',
+  }
+  const response = await makeRequest(req)
+  logger.debug({statusCode: response.statusCode, response: response.body}, 'Response')
+  return response.statusCode
+}
+
+export const updateDeployment = async (
+  tenantId: string,
+  installId: string,
+  deploymentId: string,
+  deploymentAttributes: DeploymentAttributesMetadata,
+) => {
+  const headers = {...commonHeaders, ...getAuthHeader()}
+  const apiPath = `rest/tenants/${tenantId}/brokers/installs/${installId}/deployments/${deploymentId}`
+  const config = getConfig()
+  const body = {
+    data: {
+      type: 'broker_deployment',
+      attributes: {
+        metadata: {
+          ...deploymentAttributes,
+        },
+      },
+    },
+  }
+  const req: HttpRequest = {
+    url: `${config.API_HOSTNAME}/${apiPath}?version=${config.API_VERSION}`,
+    headers: headers,
+    method: 'PATCH',
+    body: JSON.stringify(body),
   }
   const response = await makeRequest(req)
   logger.debug({statusCode: response.statusCode, response: response.body}, 'Response')
