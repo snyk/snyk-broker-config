c30c564f5c45f866efd98a1cf286247c55689a56
Antoine Arlaud <antoine@snyk.io>
2024-08-29 17:49:46 +0200

    fix: manage skippable fields and add input validation

--- src/utils/validation.ts
+++ src/utils/validation.ts
@@ -2,3 +2,13 @@ export function isValidUUID(val: string): boolean {
   const uuidRegex = /^[\da-f]{8}-[\da-f]{4}-4[\da-f]{3}-[89ab][\da-f]{3}-[\da-f]{12}$/i
   return uuidRegex.test(val)
 }
+
+export function isValidHostnameWithPort(hostname: string) {
+  const regex = /^(?!:\/\/)([\w-]{1,63}\.)*[\dA-Za-z][\w-]{0,62}\.[A-Za-z]{2,6}(:\d{1,5})?$/
+  return regex.test(hostname)
+}
+
+export function isValidUrl(url: string) {
+  const regex = /^(https?:\/\/)([\w-]{1,63}\.)*[\dA-Za-z][\w-]{0,62}\.[A-Za-z]{2,6}(:\d{1,5})?(\/.*)?$/
+  return regex.test(url)
+}
