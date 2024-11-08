a6ab09c698a7636ba4f3f8f3f749c3b4e910a85a
Pavel Sorokin <60606414+pavel-snyk@users.noreply.github.com>
2024-11-08 11:25:02 +0100

    fix: generate oclif manifest for semantic-release

--- release.config.mjs
+++ release.config.mjs
@@ -3,7 +3,23 @@
  */
 export default {
   branches: ['main'],
-  repositoryUrl: "git@github.com:snyk/snyk-broker-config.git",
-  prepare: ['@semantic-release/npm'],
+  repositoryUrl: 'git@github.com:snyk/snyk-broker-config.git',
+  prepare: [
+    '@semantic-release/npm',
+    [
+      // generate oclif.manifest.json
+      '@semantic-release/exec',
+      {
+        prepareCmd: "npx oclif manifest"
+      },
+    ],
+    [
+      // compile typescript files
+      '@semantic-release/exec',
+      {
+        prepareCmd: "npm run build"
+      },
+    ]
+  ],
   publish: ['@semantic-release/npm', '@semantic-release/github'],
 }
