ecc7b33d04ea0440fc7c9dcdb1048bd1325960ba
Antoine Arlaud <antoine@snyk.io>
2024-12-11 15:14:20 +0100

    chore: add test cases for deployments api calls

--- test/api/deployments.test.ts
+++ test/api/deployments.test.ts
@@ -1,35 +1,148 @@
-// import {expect} from 'chai'
-// import nock from 'nock'
+import {
+  createDeployment,
+  deleteDeployment,
+  DeploymentAttributesMetadata,
+  getDeployments,
+  updateDeployment,
+} from '../../src/api/deployments'
+import {expect} from 'chai'
+import nock from 'nock'
 
-// describe('Deployments Api calls', () => {
+describe('Deployments Api calls', () => {
+  const apiResponseSchema = {data: {}, links: {}}
+  before(() => {
+    process.env.SNYK_TOKEN = 'dummy'
+    nock('https://api.snyk.io')
+      .persist()
+      .get(
+        '/rest/tenants/00000000-0000-0000-0000-000000000000/brokers/installs/00000000-0000-0000-0000-000000000000/deployments?version=2024-02-08~experimental',
+      )
+      .reply((uri, body) => {
+        const response = apiResponseSchema
+        response.data = [
+          {
+            attributes: {
+              broker_app_installed_in_org_id: '00000000-0000-0000-0000-000000000000',
+              install_id: '00000000-0000-0000-0000-000000000000',
+              metadata: {},
+            },
+            id: '00000000-0000-0000-0000-000000000000',
+            type: 'broker_deployment',
+          },
+        ]
+        return [200, response]
+      })
+      .post(
+        '/rest/tenants/00000000-0000-0000-0000-000000000000/brokers/installs/00000000-0000-0000-0000-000000000000/deployments?version=2024-02-08~experimental',
+      )
+      .reply((uri, body) => {
+        const response = apiResponseSchema
+        response.data = {
+          attributes: {
+            broker_app_installed_in_org_id: '00000000-0000-0000-0000-000000000000',
+            install_id: '00000000-0000-0000-0000-000000000000',
+            metadata: {key: 'value'},
+          },
+          id: '00000000-0000-0000-0000-000000000000',
+          type: 'broker_deployment',
+        }
+        return [200, response]
+      })
+      .patch(
+        '/rest/tenants/00000000-0000-0000-0000-000000000000/brokers/installs/00000000-0000-0000-0000-000000000000/deployments/00000000-0000-0000-0000-000000000000?version=2024-02-08~experimental',
+      )
+      .reply((uri, body) => {
+        const response = apiResponseSchema
+        const attributes = {
+          broker_app_installed_in_org_id: '00000000-0000-0000-0000-000000000000',
+          install_id: '00000000-0000-0000-0000-000000000000',
+          ...JSON.parse(body.toString()).data.attributes,
+        }
+        response.data = {
+          attributes: attributes,
+          id: '00000000-0000-0000-0000-000000000000',
+          type: 'broker_deployment',
+        }
+        return [200, response]
+      })
+      .delete(
+        '/rest/tenants/00000000-0000-0000-0000-000000000000/brokers/installs/00000000-0000-0000-0000-000000000000/deployments/00000000-0000-0000-0000-000000000000?version=2024-02-08~experimental',
+      )
+      .reply(() => {
+        return [204]
+      })
+  })
 
-//   before(() => {
-//     process.env.SNYK_TOKEN = 'dummy'
-//     nock('https://api.snyk.io')
-//       .persist()
-//       .post(
-//         '/rest/tenants/00000000-0000-0000-0000-000000000000/brokers/installs/00000000-0000-0000-0000-000000000000/deployments/00000000-0000-0000-0000-000000000000/connections?version=2024-02-08~experimental',
-//       )
-//       .reply((uri, body) => {
+  it('getDeployments', async () => {
+    const gotDeployments = await getDeployments(
+      '00000000-0000-0000-0000-000000000000',
+      '00000000-0000-0000-0000-000000000000',
+    )
+    expect(gotDeployments).to.deep.equal({
+      data: [
+        {
+          attributes: {
+            broker_app_installed_in_org_id: '00000000-0000-0000-0000-000000000000',
+            install_id: '00000000-0000-0000-0000-000000000000',
+            metadata: {},
+          },
+          id: '00000000-0000-0000-0000-000000000000',
+          type: 'broker_deployment',
+        },
+      ],
+      links: {},
+    })
+  })
 
-//         return [200, {}]
-//       })
-//       .patch(
-//         '/rest/tenants/00000000-0000-0000-0000-000000000000/brokers/installs/00000000-0000-0000-0000-000000000000/deployments/00000000-0000-0000-0000-000000000000/connections/00000000-0000-0000-0000-000000000000?version=2024-02-08~experimental',
-//       )
-//       .reply((uri, body) => {
+  it('createDeployments', async () => {
+    const deploymentData: DeploymentAttributesMetadata = {
+      key: 'value',
+    }
+    const createdDeployments = await createDeployment(
+      '00000000-0000-0000-0000-000000000000',
+      '00000000-0000-0000-0000-000000000000',
+      deploymentData,
+    )
+    const expectedResponse = apiResponseSchema
+    expectedResponse.data = {
+      attributes: {
+        broker_app_installed_in_org_id: '00000000-0000-0000-0000-000000000000',
+        install_id: '00000000-0000-0000-0000-000000000000',
+        metadata: {key: 'value'},
+      },
+      id: '00000000-0000-0000-0000-000000000000',
+      type: 'broker_deployment',
+    }
 
-//         return [200, {}]
-//       })
-//       .delete(
-//         '/rest/tenants/00000000-0000-0000-0000-000000000000/brokers/installs/00000000-0000-0000-0000-000000000000/deployments/00000000-0000-0000-0000-000000000000/connections/00000000-0000-0000-0000-000000000000?version=2024-02-08~experimental',
-//       )
-//       .reply(() => {
-//         return [204]
-//       })
-//   })
-//   it('createConnectionForDeployment', async () => {
+    expect(createdDeployments).to.deep.equal(expectedResponse)
+  })
 
-//     expect(true).to.equal(true)
-//   })
-// })
+  it('updateDeployments', async () => {
+    const updatedDeployment = await updateDeployment(
+      '00000000-0000-0000-0000-000000000000',
+      '00000000-0000-0000-0000-000000000000',
+      '00000000-0000-0000-0000-000000000000',
+      {key: 'value updated'},
+    )
+    const expectedResponse = apiResponseSchema
+    expectedResponse.data = {
+      attributes: {
+        broker_app_installed_in_org_id: '00000000-0000-0000-0000-000000000000',
+        install_id: '00000000-0000-0000-0000-000000000000',
+        metadata: {key: 'value updated'},
+      },
+      id: '00000000-0000-0000-0000-000000000000',
+      type: 'broker_deployment',
+    }
+    expect(updatedDeployment).to.deep.equal(expectedResponse)
+  })
+
+  it('deleteDeployments', async () => {
+    const deletedCredentials = await deleteDeployment(
+      '00000000-0000-0000-0000-000000000000',
+      '00000000-0000-0000-0000-000000000000',
+      '00000000-0000-0000-0000-000000000000',
+    )
+    expect(deletedCredentials).to.equal(204)
+  })
+})
