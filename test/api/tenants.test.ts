26430b3e44564e258b196a16c7adb4266278c28a
Antoine Arlaud <antoine@snyk.io>
2024-12-11 15:21:22 +0100

    chore: add test cases for available tenants api calls

--- test/api/tenants.test.ts
+++ test/api/tenants.test.ts
@@ -1,31 +1,41 @@
+import {getAccessibleTenants} from '../../src/api/tenants'
 import {expect} from 'chai'
 import nock from 'nock'
 
 describe('Tenants Api calls', () => {
+  const tenants = {
+    data: [
+      {
+        attributes: {
+          created_at: 'date',
+          name: 'test tenant',
+          slug: 'test-tenant',
+          updated_at: 'date',
+        },
+        id: '00000000-0000-0000-0000-000000000000',
+        relationships: {
+          owner: {
+            data: {
+              id: '00000000-0000-0000-0000-000000000000',
+              type: '',
+            },
+          },
+        },
+        type: 'tenant',
+      },
+    ],
+  }
   before(() => {
     process.env.SNYK_TOKEN = 'dummy'
     nock('https://api.snyk.io')
       .persist()
-      .post(
-        '/rest/tenants/00000000-0000-0000-0000-000000000000/brokers/installs/00000000-0000-0000-0000-000000000000/deployments/00000000-0000-0000-0000-000000000000/connections?version=2024-02-08~experimental',
-      )
-      .reply((uri, body) => {
-        return [200, {}]
-      })
-      .patch(
-        '/rest/tenants/00000000-0000-0000-0000-000000000000/brokers/installs/00000000-0000-0000-0000-000000000000/deployments/00000000-0000-0000-0000-000000000000/connections/00000000-0000-0000-0000-000000000000?version=2024-02-08~experimental',
-      )
-      .reply((uri, body) => {
-        return [200, {}]
-      })
-      .delete(
-        '/rest/tenants/00000000-0000-0000-0000-000000000000/brokers/installs/00000000-0000-0000-0000-000000000000/deployments/00000000-0000-0000-0000-000000000000/connections/00000000-0000-0000-0000-000000000000?version=2024-02-08~experimental',
-      )
+      .get('/rest/tenants?version=2024-04-11~experimental')
       .reply(() => {
-        return [204]
+        return [200, tenants]
       })
   })
-  it('createConnectionForDeployment', async () => {
-    expect(true).to.equal(true)
+  it('getAccessibleTenants', async () => {
+    const accessibleTenants = await getAccessibleTenants()
+    expect(accessibleTenants).to.deep.equal(tenants)
   })
 })
