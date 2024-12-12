7093ed5f6bf5218b262dce59de289ddf31ee75c0
aarlaud <antoine@snyk.io>
2024-12-12 19:16:04 +0100

    chore: add credentials workflow test cases (#54)

---
+++ test/workflows/credentials/create.test.ts
@@ -0,0 +1,44 @@
+import {captureOutput} from '@oclif/test'
+import {expect} from 'chai'
+import {stdin as fstdin} from 'mock-stdin'
+
+import Credentials from '../../../src/commands/workflows/credentials/create'
+import {beforeStep, downArrow, orgId, snykToken} from '../../test-utils/nock-utils'
+import {sendScenario} from '../../test-utils/stdin-utils'
+
+const stdin = fstdin()
+
+describe('credentials workflows', () => {
+  before(beforeStep)
+
+  it('runs workflow credentials get', async () => {
+    // @ts-ignore
+    const cfg: Config = {}
+    const createCredentials = new Credentials([], cfg)
+    const {stdout, stderr, error} = await captureOutput(
+      async () => {
+        sendScenario(stdin, [snykToken, 'n', orgId, downArrow, 'ARTIFACTORY_CR_CREDS_ENV_VAR', 'test env var name'])
+        return createCredentials.run()
+      },
+      {print: false},
+    )
+    expect(stderr).to.contain('')
+    expect(error).to.be.undefined
+    expect(stdout).to.contain('Found single accessible Tenant. Using 00000000-0000-0000-0000-000000000000.')
+    expect(stdout).to.contain(
+      'Now using Tenant Id 00000000-0000-0000-0000-000000000000 and Install Id 00000000-0000-0000-0000-000000000000.',
+    )
+    expect(stdout).to.contain('Now using Deployment 00000000-0000-0000-0000-000000000000.')
+    expect(stdout).to.contain(
+      'Creating Universal Broker Credentials for Deployment 00000000-0000-0000-0000-000000000000 for Tenant 00000000-0000-0000-0000-000000000000, Install 00000000-0000-0000-0000-000000000000',
+    )
+    expect(stdout).to.contain(`- id: 00000000-0000-0000-0000-0000000000001
+        attributes:
+            comment: test env var name
+            deployment_id: 00000000-0000-0000-0000-000000000000
+            environment_variable_name: ARTIFACTORY_CR_CREDS_ENV_VAR
+            type: artifactory-cr`)
+
+    expect(stdout).to.contain('Credentials Create Workflow completed.')
+  })
+})
--- test/workflows/deployments/delete-fails.test.ts
+++ test/workflows/deployments/delete-fails.test.ts
@@ -3,28 +3,28 @@ import {expect} from 'chai'
 import {stdin as fstdin} from 'mock-stdin'
 
 import Deployments from '../../../src/commands/workflows/deployments/delete'
-import {beforeStep, orgId2, snykToken} from '../../test-utils/nock-utils'
+import {beforeStep, orgId, snykToken} from '../../test-utils/nock-utils'
 import {sendScenario} from '../../test-utils/stdin-utils'
 const stdin = fstdin()
 
 describe('deployment workflows', () => {
   before(beforeStep)
 
-  it('runs workflow deployment delete', async () => {
+  it('runs workflow deployment delete - fails if creds exist', async () => {
     // @ts-ignore
     const cfg: Config = {}
     const deleteDeployment = new Deployments([], cfg)
     const {stdout, stderr, error} = await captureOutput(
       async () => {
-        sendScenario(stdin, [snykToken, 'n', orgId2, 'y'])
+        sendScenario(stdin, [snykToken, 'n', orgId, 'y'])
 
         return deleteDeployment.run()
       },
       {print: false},
     )
     expect(stderr).to.contain('')
-    expect(error).to.be.undefined
-    expect(stdout).to.contain('Deleting deployment 00000000-0000-0000-0000-000000000002')
-    expect(stdout).to.contain('Deployment Deletion Workflow completed.')
+    expect(error?.message).to.contain(
+      'You cannot delete deployments with associated credentials references. Delete the credentials reference(s) first.',
+    )
   })
 })
