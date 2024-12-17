6354c1e4cf4074d876c04cd965795617a1eec418
Pavel Sorokin <60606414+pavel-snyk@users.noreply.github.com>
2024-12-17 17:06:43 +0100

    fix: generate oclif manifest in prepack lifecycle script

--- release.config.mjs
+++ release.config.mjs
@@ -5,21 +5,14 @@ export default {
   branches: ['main'],
   repositoryUrl: 'git@github.com:snyk/snyk-broker-config.git',
   prepare: [
-    '@semantic-release/npm',
-    [
-      // generate oclif.manifest.json
-      '@semantic-release/exec',
-      {
-        prepareCmd: "npx oclif manifest"
-      },
-    ],
     [
       // compile typescript files
       '@semantic-release/exec',
       {
-        prepareCmd: "npm run build"
+        prepareCmd: 'npm run build',
       },
-    ]
+    ],
+    '@semantic-release/npm',
   ],
   publish: ['@semantic-release/npm', '@semantic-release/github'],
 }
