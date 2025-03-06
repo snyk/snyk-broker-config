192e1739c5a6cf0080969176e4577a49129325e9
aarlaud <antoine@snyk.io>
2025-03-06 16:40:22 +0100

    chore: sort conn types and add default broker client url (#69)
    * chore: sort conn types and add default broker client url
    
    * chore: fix test cases following alpha sorting

--- test/workflows/connections/create.test.ts
+++ test/workflows/connections/create.test.ts
@@ -4,31 +4,44 @@ import {stdin as fstdin} from 'mock-stdin'
 
 import Connections from '../../../src/commands/workflows/connections/create'
 import {beforeStep, downArrow, orgId, snykToken} from '../../test-utils/nock-utils'
-import {sendScenario} from '../../test-utils/stdin-utils'
+import {sendScenarioWithOutAutoEnter} from '../../test-utils/stdin-utils'
 
-describe('deployment workflows', () => {
+describe('connection workflows', () => {
   const stdin = fstdin()
   before(beforeStep)
 
-  it('runs workflow deployment create', async () => {
+  it('runs workflow connection create', async () => {
     // @ts-ignore
     const cfg: Config = {}
     const createConnection = new Connections([], cfg)
     const {stdout, stderr, error} = await captureOutput(
       async () => {
-        sendScenario(stdin, [
+        sendScenarioWithOutAutoEnter(stdin, [
           snykToken,
+          '\n',
           'n',
+          '\n',
           orgId,
+          '\n',
           downArrow,
+          downArrow,
+          '\n',
           'test-conn',
+          '\n',
           'http://broker.client.url',
+          '\n',
           'http://artifactory.hostname',
+          '\n',
           'artifactory-crbase.hostname',
+          '\n',
           'username',
+          '\n',
           'y',
+          '\n',
           'ARTIFACTORY_PASSWORD',
+          '\n',
           'dummy comment',
+          '\n',
         ])
         return createConnection.run()
       },
