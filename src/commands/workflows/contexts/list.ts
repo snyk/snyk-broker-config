dff01b206e0041a348b8f79a73a7d20e5db76b01
Pavel Sorokin <60606414+pavel-snyk@users.noreply.github.com>
2026-02-26 16:05:24 +0100

    feat(contexts): show help for context workflows commands (#118)

--- src/commands/workflows/contexts/list.ts
+++ src/commands/workflows/contexts/list.ts
@@ -5,7 +5,6 @@ import {printFormattedJSON} from '../../../utils/display.js'
 import {getContextsForForDeployment} from '../../../api/contexts.js'
 
 export default class Workflows extends BaseCommand<typeof Workflows> {
-  static hidden = true // not yet GA
   public static enableJsonFlag = true
   static args = {
     ...commonApiRelatedArgs,
